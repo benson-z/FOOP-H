36d5611908d028a00cc123452f4456b70ade5bb8
benson-z <bensonzhou@yahoo.com>
2022-08-16 16:43:42 -0700

    Added comments and cleaned up code

--- flagchallenge.py
+++ flagchallenge.py
@@ -1,26 +1,21 @@
 # Benson Zhou - Turtle Create a Flag Challenge
 
 import turtle;
-import math;
 
+#Create turtle
 pen = turtle.Turtle()
 
+# Set turtle speed to fastest
 pen.speed(0)
 
-#draws a line
-def drawLine(angle, length):
-	turtle.pendown()
-	turtle.setheading(angle)
-	turtle.forward(length)
-
-#makes a star with a specified size
-def star(x, y, size = 1, color = "#ffffff"):
+# Draw a star
+def star(x, y, size = 1, color = "#ffffff", rotation = 0):
 	pen.penup()
 	pen.goto(x, y)
 	pen.pendown()
 	pen.color(color)
 	pen.begin_fill()
-	pen.setheading(0)
+	pen.setheading(rotation)
 	for z in range(5):
 		pen.forward(size)
 		pen.left(72)
@@ -29,45 +24,35 @@ def star(x, y, size = 1, color = "#ffffff"):
 	pen.end_fill()
 
 # Draw a rectangle
-def rectangle(x, y, length, width):
-	pen.penup()
-	pen.setheading(0)
-	pen.goto(x, y)
-	pen.pendown()
-	for x in range(2):
-		pen.forward(length)
-		pen.right(90)
-		pen.forward(width)
-		pen.right(90)
-
-#Draw a rectangle with fill
-def rectangleFill(x, y, length, width, color = "#ffffff"):
+def rectangle(x, y, length, width, color = "#ffffff", fill = False):
 	pen.penup()
 	pen.setheading(0)
 	pen.color(color)
 	pen.goto(x, y)
 	pen.pendown()
-	pen.begin_fill()
+	if fill:
+		pen.begin_fill()
 	for x in range(2):
- 		pen.forward(length)
- 		pen.right(90)
- 		pen.forward(width)
- 		pen.right(90)
-	pen.end_fill()
+		pen.forward(length)
+		pen.right(90)
+		pen.forward(width)
+		pen.right(90)
+	if fill:
+		pen.end_fill()
 
 # Flag border
-rectangle(-248, 131, 496, 262)
+rectangle(-248, 131, 496, 262, "#000000")
 
-# Red and White stripes (494 by 260)
+# Red and White stripes (494 pixels by 260 pixels)
 for stripe in range (13):
 	if stripe % 2 == 0:
 		color = "#B22234"
 	else:
 		color = "#ffffff"
-	rectangleFill(-247, 130 - stripe * 20, 494, 20, color)
+	rectangle(-247, 130 - stripe * 20, 494, 20, color, True)
 
 # Blue Rectangle
-rectangleFill(-247, 130, 198, 140, "#3C3B6E")
+rectangle(-247, 130, 198, 139, "#3C3B6E", True)
 
 # Stars
 for row in range(1, 10):
