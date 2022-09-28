bd5a8a4a5ea72b033840529b5a6bf774fdc83c8f
benson-z <bensonzhou@yahoo.com>
2022-09-28 13:15:07 -0700

    Finished Pig Latin Translator

--- piglatin/piglatin.py
+++ piglatin/piglatin.py
@@ -1,17 +1,37 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
-def translate(text):
-    return text
+vowels = ['a', 'e', 'i', 'o', 'u']
+consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
 
-class Translator(tk.TK):
-    inputText = ""
-    outputText = ""
+def translate(text, word = False):
+    if word:
+        if len(text) >= 2:
+            if text[0].lower() in consonants and text[1].lower() in vowels:
+                return text[1:] + text[0] + "ay"
+            elif text[0].lower() in consonants and text[1].lower() in consonants:
+                return text[2:] + text[:2] + "ay"
+        if text[0].lower() in vowels:
+            return text + "way"
+        return text
+    else:
+        return " ".join([translate(x, True) for x in text.split()])
+
+class Translator(tk.Tk):
+    inputText = None
+    outputText = None
     def __init__(self):
         super().__init__()
+        self.minsize(200,20)
+        self.title("Translator")
+        self.inputText = tk.StringVar()
+        ttk.Entry(master=self, width = 10, textvariable = self.inputText, validate="all", validatecommand=self.update).pack(fill="x")
+        self.outputText = ttk.Label(master=self)
+        self.outputText.pack(fill="x")
+        self.update()
     def update(self):
-        translate()
-        self.inputText.after(500, self.update)
+        self.outputText.config(text=translate(self.inputText.get()))
+        self.outputText.after(100, self.update)
 
 if __name__ == "__main__":
     app = Translator()
