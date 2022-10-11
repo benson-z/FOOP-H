73ba5ca4ae11ddaeeeacbd418daa8400859d4e71
benson-z <bensonzhou@yahoo.com>
2022-10-11 15:37:11 -0700

    update

--- shapes/shapes.py
+++ shapes/shapes.py
@@ -1,21 +1,42 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 import turtle
+import math
 
 def pinwheel(size, sides):
     global pen
+    pen.clear()
     print("Pinwheel")
-    pass
-
-def polygon(size, sides):
-    global pen
-    print("Polygon")
     pass
 
 def asterisk(size, sides):
     global pen
-    print("Asterisk")
-    pass
+    pen.clear()
+    pen.reset()
+    pen.left(90)
+    for a in range(sides):
+        pen.forward(size)
+        pen.backward(size)
+        pen.right(360/sides)
+
+def polygon(size, sides):
+    global pen
+    pen.clear()
+    pen.reset()
+    pen.left(90)
+    pen.penup()
+    for a in range(sides):
+        pen.forward(size)
+        pen.left(90)
+        pen.pendown()
+        pen.forward(math.tan(math.pi/sides)*size)
+        pen.backward(2*math.tan(math.pi/sides)*size)
+        pen.forward(math.tan(math.pi/sides)*size)
+        pen.left(90)
+        pen.penup()
+        pen.forward(size)
+        pen.right(180)
+        pen.right(360/sides)
 
 class Shapes(tk.Tk):
     selection = None
@@ -41,8 +62,8 @@ class Shapes(tk.Tk):
 
         ttk.OptionMenu(self, self.selection, *self.options).pack()
         ttk.Button(self, text="Draw", command=self.draw).pack()
-        ttk.Entry(self, textvariable=self.size).pack()
-        ttk.Entry(self, textvariable=self.sides).pack()
+        ttk.Entry(self, textvariable=self.size, validate="focusout", validatecommand=self.checkSize).pack()
+        ttk.Entry(self, textvariable=self.sides, validate="focusout", validatecommand=self.checkSides).pack()
     def draw(self):
         shape = self.selection.get()
         if shape == "Polygon":
@@ -53,7 +74,9 @@ class Shapes(tk.Tk):
             pinwheel(int(self.size.get()), int(self.sides.get()))
     def getCanvas(self):
         return self.canvas
-    def checkNum(self):
+    def checkSize(self):
+        return True
+    def checkSides(self):
         return True
 
 app = Shapes()
