2f7e3ee379fb4dab9fa771c57e04f0a889a76eb9
benson-z <bensonzhou@yahoo.com>
2022-11-18 14:37:06 -0800

    added comments

--- uniquewords/uniquewords.py
+++ uniquewords/uniquewords.py
@@ -7,6 +7,7 @@ from tkinter import filedialog as fd
 class Analyzer(tk.Tk):
     words = {}
     def __init__(self):
+        # Window initialization and layout
         super().__init__()
         self.title("Word count analyzer")
         self.resizable(False, False)
@@ -19,18 +20,21 @@ class Analyzer(tk.Tk):
         self.disp = tk.Text(self.outputFrame, state=tk.DISABLED, highlightthickness=0, width=40, height=20)
         self.disp.pack()
         ttk.Button(self.optionFrame, text="Open file", command=self.openFile).pack(padx=5)
-    # a-z,A-Z, ', -
     def index(self, word):
+        # match a-z,A-Z, ', -
+        # Deletes all punctuation and numbers
         word = re.sub("[^a-zA-Z'-]", "", word)
-        if not word in self.words:
+        if not word in self.words and not word == "":
             self.words[word] = 0
         self.words[word] += 1
+    # Asks the user for a file to open via filedialog and processes the contents
     def openFile(self):
         self.words = {}
         with open(fd.askopenfilename()) as f:
             for line in f:
                 [self.index(x.lower()) for x in line.split()]
         self.updateDisp()
+    # Output the final count to the application
     def updateDisp(self):
         outputText = ""
         word_count = 0
@@ -43,5 +47,6 @@ class Analyzer(tk.Tk):
         self.disp.insert("end", outputText)
         self.disp.config(state=tk.DISABLED)
 
+# Make application instance
 app = Analyzer()
 app.mainloop()
