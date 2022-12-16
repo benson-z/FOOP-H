07db1a182024440780e2ae399660ddb8d827133f
benson-z <bensonzhou@yahoo.com>
2022-12-15 23:28:47 -0800

    classwork

--- Code/stringpractice.py
+++ Code/stringpractice.py
@@ -1,4 +1,6 @@
 # Exercise 9: Calculate the sum and average of the digits present in a string
+str1 = "PYnative29@#8496"
+
 total = 0
 count = 0
 def getInt(x):
@@ -8,14 +10,33 @@ def getInt(x):
         count += 1 
     except:
         return
-[getInt(a) for a in input("Exercise 9: Calculate the sum and average of the digits present in a string\nInput String: ")]
+[getInt(a) for a in str1]
 print("Sum is:", total, "Average is", total/count)
 
 # Exercise 10: Write a program to count occurrences of all characters within a string
+str1 = "Apple"
 occurrences = {}
-[occurrences[a] += 1 for a in input()]
+def count(char):
+    if not char in occurrences:
+        occurrences[char] = 0
+    occurrences[char] += 1
+[count(a) for a in str1]
+print(occurrences)
+
+# Exercise 11: Reverse a given string
+str1 = "PYnative"
+list1 = list(str1)
+list1.reverse()
+print("".join(list1))
+
+# Exercise 13: Split a string on hyphens
+
+str1 = "Emma-is-a-data-scientist"
+[print(x) for x in str1.split("-")]
 
 # Exercise 16: Removal all characters from a string except integers
+str1 = 'I am 25 years and 10 months old'
+
 output = ""
 
 def isInt(x):
@@ -26,6 +47,6 @@ def isInt(x):
     except:
         return
 
-[isInt(a) for a in input("Exercise 16: Removal all characters from a string except integers\nInput String: ")]
+[isInt(a) for a in str1]
 
 print(output)
